2b71783f2dd85cd5278d3fd6050531d509f9e9db
Gpan <georgepanagopoulos5@yahoo.gr>
2019-10-12 17:49:03 +0200

    fixed arguements

--- main.py
+++ main.py
@@ -6,45 +6,54 @@
 import time
 import os
 import sys
-"""
+import argparse
 import extract_feats_and_trainset
 import preprocess_for_imm
 import rank_nodes
 import infector
 import iminfector
 import evaluation
-"""
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--sampling_perc', help='')
+parser.add_argument('--learning_rate', help='')
+parser.add_argument('--n_epochs', help='')
+parser.add_argument('--embedding_size', help='')
+parser.add_argument('--num_neg_samples', help='')
+
+
 
 if __name__ == '__main__':
 	start = time.time()
 	abspath = os.path.abspath(__file__)
 	dname = os.path.dirname(abspath)
 	os.chdir(os.path.join(dname,"..","Data"))
+	args=parser.parse_args()
 	
-	print(sys.argv[1])
 	#--- Parameters
-	if(len(sys.argv[1])>0):
-		sampling_perc = int(sys.argv[1])
+	if(len(args.sampling_perc)>0):#sys.argv[1])>0):
+		sampling_perc = int(args.sampling_perc)
 	else:
 		sampling_perc = 120	
 		
-	if(len(sys.argv[2])>0):
-		learning_rate = float(sys.argv[2])
+	if(len(args.learning_rate)>0):
+		learning_rate = float(args.learning_rate)
 	else:
 		learning_rate = 0.1
 
-	if(len(sys.argv[3])>0):
-		n_epochs = int(sys.argv[3])
+	if(len(args.n_epochs)>0):
+		n_epochs = int(args.n_epochs)
 	else:
 		n_epochs = 5
 
-	if(len(sys.argv[4])>0):
-		embedding_size = int(sys.argv[4])
+	if(len(args.embedding_size)>0):
+		embedding_size = int(args.embedding_size)
 	else:
 		embedding_size = 50
 
-	if(len(sys.argv[5])>0):
-		num_neg_samples = int(sys.argv[5])
+	if(len(args.num_neg_samples)>0):
+		num_neg_samples = int(args.num_neg_samples)
 	else:
 		num_neg_samples = 10
 
